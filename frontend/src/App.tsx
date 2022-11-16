4e8d3d235bbf2ba35db363351c49f3ba7094cada
CodeThales <thalles.agency@gmail.com>
2022-11-16 20:43:55 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/NotificationButton/Header"
+import SalesCard from "./components/SalesCard"
+import '../src/index.css'
 
 function App() {
   return (
     <>
-      <h1>Hello, World!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>      
     </>
   )
 }
